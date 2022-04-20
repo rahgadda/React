dab5d00eed103c302420737570eb567bd0f079ca
rahgadda <gaddam.rahul@gmail.com>
2022-04-20 05:19:26 +0000

    Initial Draft

--- 02-MyDev/00/electron/src/App.js
+++ 02-MyDev/00/electron/src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
-import React, { Component } from 'react'
-import NavBar from './components/NavBar'
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <NavBar />
-      </div>
-    )
-  }
+function App() {
+  return (
+    <div>Hello World</div>
+  );
 }
 
-export default App
+export default App;
